ce7f0c9b9fbaaae291cd1d019ded6555e2ae73c0
mariyam <khanmariyam40886@gmail.com>
2021-12-01 01:49:15 -0800

    third file

---
+++ simpleif.js
@@ -0,0 +1,10 @@
+//Print examination result of a student:"Pass" or "Fail"
+//Min Pass Marks:35 out of 100
+var securedMarks = 60;
+if (securedMarks < 35) {
+    console.log("Fail");
+}
+
+if (securedMarks >= 35) {
+    console.log("Pass");
+}
